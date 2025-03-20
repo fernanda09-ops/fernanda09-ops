73eb008a2ad2f9056336cd2782a49d2fd01a8bb0
fernanda09-ops <fcaetanoamorim@gmail.com>
2025-03-20 08:23:47 -0300

    Create impressao2.py

---
+++ impressao2.py
@@ -0,0 +1,5 @@
+
+pessoa1 = ['josielly', '06471991738', '16']
+pessoa2 = ['maria fernanda', '11357991769', '18']
+pessoa3 = ['charles', '54646546545', '32']
+pessoa4 = ['fernanda', '23456798056', '17']
